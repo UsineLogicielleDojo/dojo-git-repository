95de7344a33cb7a436602c0fc06c0f3e614f6334
UsineLogicielleDojo <laure.garda@gmail.com>
2017-06-01 13:13:57 +0200

    ajout code source

---
+++ com.thalesgroup.calculator/src/test/java/test/CalculatorViewTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CalculatorViewTest {
+	  @Before
+	  public void init() throws Exception {
+	    System.out.println("Setting up ...");
+	  }
+	 
+	  @After
+	  public void destroy() throws Exception {
+	    System.out.println("Tearing down ...");
+	  }
+	 
+	  @Test
+	  public void test1() {
+	    Assert.assertNull(null);
+	  }
+	  
+	  @Test
+	  public void test2() {
+	    Assert.assertNull(5);
+	  }
+}
---
+++ com.thalesgroup.calculatorModel/src/test/java/test/CalculatorModelTest.java
@@ -0,0 +1,19 @@
+package test;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+
+public class CalculatorModelTest extends TestCase {
+	  
+	  public void setUp() throws Exception {
+	    System.out.println("Setting up ...");
+	  }
+	 
+	  public void tearDown() throws Exception {
+	    System.out.println("Tearing down ...");
+	  }
+	 
+	  public void testGetBestTeam() {
+	    Assert.assertNull(null);
+	  }
+}
